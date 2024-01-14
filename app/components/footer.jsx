67d7ee5dbe0602ab48f38a7a5829db5517f62404
Allan Fernandes <allan.fernandes@responsiblelife.co.uk>
2024-01-14 14:32:49 +0000

    Update footer component and global styles

--- app/components/footer.jsx
+++ app/components/footer.jsx
@@ -8,38 +8,67 @@ const Footer = () => {
         twitter: 'https://twitter.com/creatalysis',
         instagram: 'https://www.instagram.com/creatalysis/',
         medium: 'https://medium.com/@creatalysis',
-        whatsapp: 'https://wa.link/m7bnct'
+        whatsapp: 'https://wa.link/m7bnct',
+        knowBrand: 'https://www.figma.com/community/file/1290112742882943529/creatalysis'
     }
 
     return (
-    <footer className='footer-griddy container mx-auto'>
-        <div className="flex flex-col justify-between gap-[1rem]">
+    <footer className='footer-griddy container mx-auto mb-12'>
+        {/* Footer Left */}
+        <div className="flex flex-col md:justify-between justify-center gap-[1rem]">
             <Image src='/images/footer-logo.svg'
                 alt='users'
                 width={100}
                 height={100}
-                className='w-[150px]'/>
+                className='w-[150px] md:m-0 m-auto'/>
             {/* Comic download btn */}
             <Link href='/'>
-               <div className='bg-createYellow p-2 shadow-create flex items-center justify-between gap-2'>
-                <div>
-                    <p className='text-base font-bold'>Download our short comic</p>
-                    <p className='text-sm text-createGray italic'>Check out our short comic – it's quite the read!</p>
+            <div className='bg-createYellow p-2 shadow-create flex items-center justify-between gap-2 px-6 py-3 md:w-auto w-[75%] m-auto relative clicktodownload'>
+                <div className='mt-6'>
+                    <p className='text-base font-bold'>Check out our short comic</p>
+                    <p className='text-sm text-createGray italic'>It's quite the read!</p>
                 </div>
-                <div>
-                    <Image src='/images/download.png'
-                        alt='users'
-                        width={100}
-                        height={80}
-                        className='w-[50px]'/>
-                </div>
-               </div>
+            </div>
             </Link>
         </div>
-        <div>
-
+        <div className='flex justify-around'>
+            {/* left links */}
+            <div className='flex flex-col gap-[1rem] text-base'>
+                <Link href='/'>
+                    <p className='text-createGray cursor-pointer'>Home</p>
+                </Link>
+                <Link href='/about'>
+                    <p className='text-createGray cursor-pointer'>About Us</p>
+                </Link>
+                <Link href='/contact'>
+                    <p className='text-createGray cursor-pointer'>Earn with us</p>
+                </Link>
+                <Link href='/contact'>
+                    <p className='text-createGray cursor-pointer'>Contact Us</p>
+                </Link>
+                <Link href={links.medium}>
+                    <p className='text-createGray cursor-pointer'>Blogs</p>
+                </Link>
+                <Link href={links.knowBrand}>
+                    <p className='text-createGray cursor-pointer'>Know our brand</p>
+                </Link>
+            </div>
+            {/* right links */}
+            <div className='flex flex-col gap-[1rem] text-base justify-between'>
+                <div className='flex flex-col'>
+                <p className='mb-2'><strong>Reach us</strong></p>
+                    <Link href='mailto:hello@creatalysis.com'>hello@creatalysis.com</Link>
+                    <Link href='tel:+91 79001 76277'>+91 79001 76277</Link>
+                </div>
+                <div className='flex flex-col'>
+                    <p className='mb-2'><strong>Careers</strong><span className='bg-createYellow rounded-full text-sm px-2 py-1 ml-2'>We are hiring!!!</span></p>
+                    <p>Send your CVs to</p>
+                    <Link href='mailto:hello@creatalysis.com'>hello@creatalysis.com</Link>
+                </div>
+            </div>
         </div>
-        <div className="flex flex-col gap-[1rem] ml-auto">
+        {/* Social Links */}
+        <div className="flex flex-col gap-[1rem] lg:mr-auto mx-auto">
             <p className='text-base font-bold'>Our Social presence</p>
             <div className='flex items-center footer-social-grid'>
                 <Link href={links.linkedin}>
@@ -84,13 +113,3 @@ const Footer = () => {
 }
 
 export default Footer
-/* Links
-https://medium.com/@creatalysis
-https://www.figma.com/community/file/1290112742882943529
-https://www.linkedin.com/company/creatalysis-studio/
-https://twitter.com/creatalysis
-https://www.instagram.com/creatalysis/
-
-+91 7900176277 
-
-*/
